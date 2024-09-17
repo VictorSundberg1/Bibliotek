7e9b26386723fd11496c3883414d22155d99e529
VictorSundberg1 <victor.sundberg1@hotmail.com>
2024-09-17 13:06:00 +0200

    List and some methods done

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,46 @@
 public class Book {
+    String bookName;
+    String author;
+    String bookYear;
+    String bookEdition;
+    boolean availability = true;
+
+    public Book(String bookName, String author, String bookYear, String bookEdition) {
+        this.bookName = bookName;
+        this.author = author;
+        this.bookYear = bookYear;
+        this.bookEdition = bookEdition;
+
+    }
+
+
+
+    public void loanBook() {
+        if (availability == true){
+            availability = false;
+            System.out.println( "You have loaned " + bookName);
+        }else {
+            System.out.println("The book is unavailable");
+        }
+    }
+
+    public void returnBook() {
+        if (availability == false){
+            availability = true;
+            System.out.println( "You have returned " + bookName);
+        }else {
+            System.out.println("The book is already available");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookName='" + bookName + '\'' +
+                ", author='" + author + '\'' +
+                ", bookYear='" + bookYear + '\'' +
+                ", bookEdition='" + bookEdition + '\'' +
+                ", availability=" + availability +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Book b1 = new Book("Harry Potter: Philosopher's Stone", "J.K Rowling", "1997", "1st");
+        Book b2 = new Book("Harry Potter: Chamber of Secrets", "J.K Rowling", "1998", "1st");
+
+        ArrayList<Book> books = new ArrayList<>();
+        
+        books.add(b1);
+        books.add(b2);
+
+        for (Book book : books){
+            System.out.println(book);
+        }
+        b1.returnBook();
+        b1.loanBook();
+        b1.loanBook();
+        b1.returnBook();
+        b1.returnBook();
+
     }
 }
